7ac81471e3a4077d7dd652d24b80f90b06f1b865
Mohamed MANSARE <mohamed.mansare-ext@ekino.com>
2020-08-08 15:31:54 +0200

    [tuto][angular][udemy] First Assignment

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: '.app-warning',
+  template: `
+    <img src="../../assets/images/warning-icon.png" alt="Warning !!!">
+  `
+})
+export class WarningComponent {
+
+}
